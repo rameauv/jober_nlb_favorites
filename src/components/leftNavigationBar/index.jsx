9691df4a024a91a4ac8b506032054067270ddab0
valentin rameau <valentin.rameau@outlook.fr>
2023-03-03 13:55:26 +0900

    initial commit

--- src/components/leftNavigationBar/index.jsx
+++ src/components/leftNavigationBar/index.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { Menu } from "antd";
+import {Menu} from "antd";
 import MENUS from "./menus";
-import "./index.css";
+import styles from "./index.module.css"
+import {StarOutlined, StarFilled} from "@ant-design/icons";
 
 const LeftNavigationBar = () => {
   return (
-    <div className="left-navigation-bar">
-      <Menu defaultSelectedKeys={[MENUS[0].key]} mode="inline" theme="dark">
-        {MENUS.map(({ key, label, icon }) => (
-          <Menu.Item key={key} icon={icon}>
-            {label}
-          </Menu.Item>
-        ))}
-      </Menu>
-    </div>
+      <div className={styles.leftBavigationBar}>
+        <Menu
+            className={styles.menu}
+            defaultSelectedKeys={[MENUS[0].key]}
+            mode="inline"
+            theme="dark"
+        >
+          {MENUS.map(({key, label, icon}) => (
+              <Menu.Item key={key} icon={icon}>
+                <span>{label}</span>
+                <StarOutlined/>
+              </Menu.Item>
+          ))}
+        </Menu>
+      </div>
   );
 };
 
