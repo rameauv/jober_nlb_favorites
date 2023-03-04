a7f7eb2f30406534709dcdf0bf9c5e9162083055
valentin rameau <valentin.rameau@outlook.fr>
2023-03-05 07:09:49 +0900

    fea(LNB): add another test

--- src/components/leftNavigationBar/indexLeftNavigationBar.cy.jsx
+++ src/components/leftNavigationBar/indexLeftNavigationBar.cy.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import LeftNavigationBar from './index'
 
+const addToFavoritesButtonSelector = "[data-cy=\"add-to-favorites-button\"]";
+const removeFromFavoritesButtonSelector = "[data-cy=\"remove-from-favorites-button\"]";
+
 describe('<LeftNavigationBar />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
@@ -8,13 +11,30 @@ describe('<LeftNavigationBar />', () => {
   });
 
   it('shoul add 3 items to the favorites', () => {
-    let menuItemCount = 0;
-
     cy.mount(<LeftNavigationBar/>)
-    cy.get('[data-cy="add-to-favorites-button"]').then((value) => {
-      menuItemCount = Cypress.$(value).length;
+    cy.get(addToFavoritesButtonSelector).then((value) => {
+      const menuItemCount = Cypress.$(value).length;
+
+      cy.get(value).each(() => {
+        cy.get(addToFavoritesButtonSelector).first().click();
+      });
+      cy.get(removeFromFavoritesButtonSelector).should('have.length', menuItemCount);
+      cy.get(addToFavoritesButtonSelector).should('have.length', 0);
     });
-    cy.get('[data-cy="add-to-favorites-button"]').first(element => cy.get(element).click());
-    cy.get('[data-cy="remove-from-favorites-button"]').should('have.length', menuItemCount);
+  });
+
+  it('shoul add and remove 3 items to the favorites', () => {
+    cy.mount(<LeftNavigationBar/>)
+    cy.get(addToFavoritesButtonSelector).then((value) => {
+      const menuItemCount = Cypress.$(value).length;
+      cy.get(addToFavoritesButtonSelector).each(() => {
+        cy.get(addToFavoritesButtonSelector).first().click();
+      });
+      cy.get(removeFromFavoritesButtonSelector).each(() => {
+        cy.get(removeFromFavoritesButtonSelector).first().click();
+      });
+      cy.get(addToFavoritesButtonSelector).should('have.length', menuItemCount);
+      cy.get(removeFromFavoritesButtonSelector).should('have.length', 0);
+    });
   });
 })
